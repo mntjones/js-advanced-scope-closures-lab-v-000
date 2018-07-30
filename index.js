b85165d9f300feaebbd955e677a890ffa475e740
Monica Tamaru Jones <mntamaru@gmail.com>
2018-07-30 01:09:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ function produceDrivingRange(blocks) {
     const distance = Math.abs(parseInt(starting)-parseInt(ending));
     
     if (distance > blocks) {
-    return `${distance-blocks} blocks out of range`;
-   }
-    else {return `within range by ${blocks-distance}`;}
-    
+      return `${distance-blocks} blocks out of range`;
+    }
+    else {
+      return `within range by ${blocks-distance}`;
+    }
   }
 }
 
-function produceTipCalculator() {
-  
+function produceTipCalculator(rate) {
+  return function(fare) {
+    return 
+  }
 }
 
 function createDriver() {
