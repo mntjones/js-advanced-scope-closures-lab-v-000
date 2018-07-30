fd577bad9f9a8a1b4bfec0883f6c3ad48042f197
Monica Tamaru Jones <mntamaru@gmail.com>
2018-07-30 01:12:47 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+function produceDrivingRange(blocks) {
+  return function(starting, ending) {
+    const distance = Math.abs(parseInt(starting)-parseInt(ending));
+    
+    if (distance > blocks) {
+      return `${distance-blocks} blocks out of range`;
+    }
+    else {
+      return `within range by ${blocks-distance}`;
+    }
+  }
+}
+
+function produceTipCalculator(rate) {
+  return function(fare) {
+    return fare * rate;
+  }
+}
+
+function createDriver() {
+  let driverId = 0;
+  
+  return class {
+    constructor (name) {
+      this.name = name;
+      this.id = ++driverId;
+    }
+  }
+}
