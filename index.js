c14c726ed5ab4b9ee97c79a377ba1af17441e253
Monica Tamaru Jones <mntamaru@gmail.com>
2018-07-30 01:02:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function produceDrivingRange(blocks) {
   return function(starting, ending) {
     const distance = Math.abs(starting-ending);
     if (distance > blocks) {
-    return ``;
+    return `${distance} blocks out of range.`;
   }
   else {return true;}
     
