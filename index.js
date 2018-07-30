28997b20c6e962daf5a789d98c9e79407e7cf2b8
Monica Tamaru Jones <mntamaru@gmail.com>
2018-07-30 01:12:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ function createDriver() {
   let driverId = 0;
   
   return class {
-    constructor () {
+    constructor (name) {
+      this.name = name;
       this.id = ++driverId;
     }
   }
